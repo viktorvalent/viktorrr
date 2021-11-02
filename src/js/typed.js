56bc26c9f411a47bda9434d092032fa85fc1ee32
viktorvalent <viktorvalents@gmail.com>
2021-11-02 19:58:57 +0700

    init 2

---
+++ src/js/typed.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".nama", {
+    strings: ['oius Valntn', 'oris Valent', 'orius Valentino.'],
+    typeSpeed: 70,
+    startDelay: 90,
+    loop: false
+})
--- src/js/typeit.js
+++
@@ -1,5 +0,0 @@
-new TypeIt("#name", {
-    strings: "Viktorius Valentino",
-    speed: 50,
-    waitUntilVisible: true,
-  }).go();
